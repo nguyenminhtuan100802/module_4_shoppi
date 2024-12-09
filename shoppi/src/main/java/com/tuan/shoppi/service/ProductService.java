2ccacabcb476e4b80796e5b307a79223ba215ffd
nguyenminhtuan100802 <nguyenminhtuan100802@gmail.com>
2024-12-09 15:23:01 +0700

    save

---
+++ shoppi/src/main/java/com/tuan/shoppi/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.tuan.shoppi.service;
+
+import com.tuan.shoppi.entity.Product;
+import com.tuan.shoppi.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class ProductService {
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    List<Product> findAll() {
+        return productRepository.findAll();
+    }
+    List<Product> findByNameContainingIgnoreCase(String name) {
+        return productRepository.findByNameContainingIgnoreCase(name);
+    }
+    private Set<Product> findByCategory(Category category) {}
+}
